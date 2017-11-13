839dd4f2ad55c7e70032f25db4c676ec8d9d0b32
zentino <sn-z777@outlook.com>
2017-11-13 20:51:53 +0100

    35-72

--- koans/about_hashes.rb
+++ koans/about_hashes.rb
@@ -8,6 +8,8 @@ class AboutHashes < Neo::Koan
     assert_equal 0, empty_hash.size
   end
 
+  # You can create hashes using {}
+
   def test_hash_literals
     hash = { :one => "uno", :two => "dos" }
     assert_equal 2, hash.size
@@ -30,7 +32,18 @@ class AboutHashes < Neo::Koan
     # THINK ABOUT IT:
     #
     # Why might you want to use #fetch instead of #[] when accessing hash keys?
-    
+
+    # https://stackoverflow.com/questions/16569409/fetch-vs-when-working-with-hashes
+    #     By default, using #[] will retrieve the hash value if it exists, and return nil if it doesn't exist *.
+    #
+    #     Using #fetch gives you a few options (see the docs on #fetch):
+    #
+    #     fetch(key_name): get the value if the key exists, raise a KeyError if it doesn't
+    #     fetch(key_name, default_value): get the value if the key exists, return default_value otherwise
+    #     fetch(key_name) { |key| "default" }: get the value if the key exists, otherwise run the supplied block and return the value.
+    #
+    #     Each one should be used as the situation requires, but #fetch is very feature-rich and can handle many cases depending on how it's used.
+    #     For that reason I tend to prefer it over accessing keys with #[].
 
   end
 
@@ -38,8 +51,8 @@ class AboutHashes < Neo::Koan
     hash = { :one => "uno", :two => "dos" }
     hash[:one] = "eins"
 
-    expected = { :one => __, :two => "dos" }
-    assert_equal __, hash
+    expected = { :one => "eins", :two => "dos" }
+    assert_equal expected, hash
 
     # Bonus Question: Why was "expected" broken out into a variable
     # rather than used as a literal?
@@ -49,70 +62,113 @@ class AboutHashes < Neo::Koan
     hash1 = { :one => "uno", :two => "dos" }
     hash2 = { :two => "dos", :one => "uno" }
 
-    assert_equal __, hash1 == hash2
+    assert_equal true, hash1 == hash2
   end
+
+  # Den Inhalt zweier Objekte vergleicht man in Ruby mit "==".
 
   def test_hash_keys
     hash = { :one => "uno", :two => "dos" }
-    assert_equal __, hash.keys.size
-    assert_equal __, hash.keys.include?(:one)
-    assert_equal __, hash.keys.include?(:two)
-    assert_equal __, hash.keys.class
+    assert_equal 2, hash.keys.size
+    assert_equal true, hash.keys.include?(:one)
+    assert_equal true, hash.keys.include?(:two)
+    assert_equal Array, hash.keys.class
   end
+  # include?(key)"Returns true if the given key is present in hsh."
+  # hash.keys returns an array with the keys.
 
   def test_hash_values
     hash = { :one => "uno", :two => "dos" }
-    assert_equal __, hash.values.size
-    assert_equal __, hash.values.include?("uno")
-    assert_equal __, hash.values.include?("dos")
-    assert_equal __, hash.values.class
+    assert_equal 2, hash.values.size
+    assert_equal true, hash.values.include?("uno")
+    assert_equal true, hash.values.include?("dos")
+    assert_equal Array, hash.values.class
   end
+
+  # "values" returns the values from the hash.
+  # include?(value) returns true if the given value is present in hash.
 
   def test_combining_hashes
     hash = { "jim" => 53, "amy" => 20, "dan" => 23 }
     new_hash = hash.merge({ "jim" => 54, "jenny" => 26 })
 
-    assert_equal __, hash != new_hash
+    assert_equal true, hash != new_hash
 
-    expected = { "jim" => __, "amy" => 20, "dan" => 23, "jenny" => __ }
-    assert_equal __, expected == new_hash
+    expected = { "jim" => 54, "amy" => 20, "dan" => 23, "jenny" => 26 }
+    assert_equal true, expected == new_hash
   end
+
+      # merge(other_hash)
+      # merge(other_hash){|key, oldval, newval| block}
+      #
+      # Returns a new hash containing the contents of other_hash and the contents of hsh.
+      # If no block is specified, the value for entries with duplicate keys will be that of other_hash. Otherwise
+      # the value for each duplicate key is determined by calling the block with the key, its value in hsh and its value in other_hash.
 
   def test_default_value
     hash1 = Hash.new
     hash1[:one] = 1
 
-    assert_equal __, hash1[:one]
-    assert_equal __, hash1[:two]
+    assert_equal 1, hash1[:one]
+    assert_equal nil, hash1[:two]
 
     hash2 = Hash.new("dos")
+    # hash2 -> {}
+    # hash2.keys -> {}
     hash2[:one] = 1
+    # hash2.keys -> [:one]
 
-    assert_equal __, hash2[:one]
-    assert_equal __, hash2[:two]
+    assert_equal 1, hash2[:one]
+    assert_equal "dos", hash2[:two]
+    # instead of returning nil it returns the default object.
   end
+
+    # new -> new_hash
+    # new.(obj) -> new_hash
+    # Returns a new, empty hash. If this hash is subsequently accessed by a key that doesn't correspond to a hash entry,
+    # the value returned depends on the style of new used to create the hash.
+    # In the first form, the access returns nil. If obj is specified, this single object will be used for all default values.
+    # The default object in this example is "dos". hash2.keys -> [:one]
+
 
   def test_default_value_is_the_same_object
     hash = Hash.new([])
 
     hash[:one] << "uno"
     hash[:two] << "dos"
+    # hash[:four] -> ["uno","dos"]
+    # hash.keys -> []
+    # hash[:four] = 1
+    # hash.keys -> [:four]
 
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
+    assert_equal ["uno","dos"], hash[:one]
+    assert_equal ["uno","dos"], hash[:two]
+    assert_equal ["uno","dos"], hash[:three]
 
-    assert_equal __, hash[:one].object_id == hash[:two].object_id
+    assert_equal true, hash[:one].object_id == hash[:two].object_id
   end
+
+     #  ary << obj → ary
+     # Append—Pushes the given object on to the end of this array.
+     # This expression returns the array itself, so several appends may be chained together.
+     # hash = Hash.new("dos")
+     # hash[:one]
+     # => "dos" default object
+     # hash[:one] << "uno"
+     # hash[:one] << "dosuno"
+     # if the value is 1 (integer) then 1 << 2 -> 4. The binary representation of 1 gets shifted to the left 2 times (0001 -> 0100)
 
   def test_default_value_with_block
     hash = Hash.new {|hash, key| hash[key] = [] }
 
     hash[:one] << "uno"
     hash[:two] << "dos"
-
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
+    assert_equal [:one,:two], hash.keys
+    assert_equal ["uno"], hash[:one]
+    assert_equal ["dos"], hash[:two]
+    assert_equal [], hash[:three]
   end
+
+  # If a block is specified, it will be called with the hash object and the key,
+  # and should return the default value. It is the block's responsibility to store the value in the hash if required.
 end
--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -3,36 +3,43 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutStrings < Neo::Koan
   def test_double_quoted_strings_are_strings
     string = "Hello, World"
-    assert_equal __, string.is_a?(String)
+    assert_equal true, string.is_a?(String)
   end
 
   def test_single_quoted_strings_are_also_strings
     string = 'Goodbye, World'
-    assert_equal __, string.is_a?(String)
+    assert_equal true, string.is_a?(String)
   end
 
   def test_use_single_quotes_to_create_string_with_double_quotes
     string = 'He said, "Go Away."'
-    assert_equal __, string
+    assert_equal "He said, \"Go Away.\"", string
   end
 
   def test_use_double_quotes_to_create_strings_with_single_quotes
     string = "Don't"
-    assert_equal __, string
+    assert_equal "Don\'t", string
   end
 
   def test_use_backslash_for_those_hard_cases
     a = "He said, \"Don't\""
     b = 'He said, "Don\'t"'
-    assert_equal __, a == b
+    assert_equal true, a == b
   end
 
   def test_use_flexible_quoting_to_handle_really_hard_cases
     a = %(flexible quotes can handle both ' and " characters)
     b = %!flexible quotes can handle both ' and " characters!
     c = %{flexible quotes can handle both ' and " characters}
-    assert_equal __, a == b
-    assert_equal __, a == c
+    # a = %{"'}­
+    # => "\"'"
+    # > a = %(fle­xible quote­s can handl­e both ' and " chara­cters)
+    # => "flexible quotes can handle both ' and \" characters"
+    # > b = %!fle­xible quote­s can handl­e both ' and " chara­cters!
+    # => "flexible quotes can handle both ' and \" characters"
+
+    assert_equal true, a == b
+    assert_equal true, a == c
   end
 
   def test_flexible_quotes_can_handle_multiple_lines
@@ -40,9 +47,9 @@ class AboutStrings < Neo::Koan
 It was the best of times,
 It was the worst of times.
 }
-    assert_equal __, long_string.length
-    assert_equal __, long_string.lines.count
-    assert_equal __, long_string[0,1]
+    assert_equal 54, long_string.length
+    assert_equal 3, long_string.lines.count
+    assert_equal "\n", long_string[0,1]
   end
 
   def test_here_documents_can_also_handle_multiple_lines
@@ -50,29 +57,29 @@ It was the worst of times.
 It was the best of times,
 It was the worst of times.
 EOS
-    assert_equal __, long_string.length
-    assert_equal __, long_string.lines.count
-    assert_equal __, long_string[0,1]
+    assert_equal 53, long_string.length
+    assert_equal 2, long_string.lines.count
+    assert_equal "I", long_string[0,1]
   end
 
   def test_plus_will_concatenate_two_strings
     string = "Hello, " + "World"
-    assert_equal __, string
+    assert_equal "Hello, World", string
   end
 
   def test_plus_concatenation_will_leave_the_original_strings_unmodified
     hi = "Hello, "
     there = "World"
     string = hi + there
-    assert_equal __, hi
-    assert_equal __, there
+    assert_equal "Hello, ", hi
+    assert_equal "World", there
   end
 
   def test_plus_equals_will_concatenate_to_the_end_of_a_string
     hi = "Hello, "
     there = "World"
     hi += there
-    assert_equal __, hi
+    assert_equal "Hello, World", hi
   end
 
   def test_plus_equals_also_will_leave_the_original_string_unmodified
@@ -80,15 +87,15 @@ EOS
     hi = original_string
     there = "World"
     hi += there
-    assert_equal __, original_string
+    assert_equal "Hello, ", original_string
   end
 
   def test_the_shovel_operator_will_also_append_content_to_a_string
     hi = "Hello, "
     there = "World"
     hi << there
-    assert_equal __, hi
-    assert_equal __, there
+    assert_equal "Hello, World", hi
+    assert_equal "World", there
   end
 
   def test_the_shovel_operator_modifies_the_original_string
@@ -96,86 +103,118 @@ EOS
     hi = original_string
     there = "World"
     hi << there
-    assert_equal __, original_string
+    assert_equal "Hello, World", original_string
 
     # THINK ABOUT IT:
     #
     # Ruby programmers tend to favor the shovel operator (<<) over the
     # plus equals operator (+=) when building up strings.  Why?
+
+    # https://stackoverflow.com/questions/4684446/why-is-the-shovel-operator-preferred-over-plus-equals-when-building-a
+
+    # a = 'foo'
+    # a.object_id #=> 2154889340
+    # a << 'bar'
+    # a.object_id #=> 2154889340
+    # a += 'quux'
+    # a.object_id #=> 2154742560
+    #
+    # So << alters the original string rather than creating a new one. The reason for this is that in ruby a += b is syntactic shorthand for
+    # a = a + b (the same goes for the other <op>= operators) which is an assignment.
+    # On the other hand << is an alias of concat() which alters the receiver in-place.
+
+    # A friend who is learning Ruby as his first programming language asked me this same question while going through Strings in Ruby
+    # on the Ruby Koans series. I explained it to him using the following analogy;
+    # You have a glass of water that is half full and you need to refill your glass.
+    #
+    # First way you do it by taking a new glass, filling it halfway with water from a tap and then using this second half-full
+    # glass to refill your drinking glass. You do this every time you need to refill your glass.
+    #
+    # The second way you take your half full glass and just refill it with water straight from the tap.
+    #
+    # At the end of the day, you would have more glasses to clean if you choose to pick a new glass every time you needed to refill your glass.
+    #
+    # The same applies to the shovel operator and the plus equal operator. Plus equal operator picks a new 'glass' every time
+    # it needs to refill its glass while the shovel operator just takes the same glass and refills it. At the end of the day more 'glass' collection for the Plus equal operator.
+
+
+
   end
 
   def test_double_quoted_string_interpret_escape_characters
     string = "\n"
-    assert_equal __, string.size
+    assert_equal 1, string.size
   end
 
   def test_single_quoted_string_do_not_interpret_escape_characters
     string = '\n'
-    assert_equal __, string.size
+    assert_equal 2, string.size
   end
 
   def test_single_quotes_sometimes_interpret_escape_characters
     string = '\\\''
-    assert_equal __, string.size
-    assert_equal __, string
+    assert_equal 2, string.size
+    assert_equal "\\'", string
   end
 
   def test_double_quoted_strings_interpolate_variables
     value = 123
     string = "The value is #{value}"
-    assert_equal __, string
+    assert_equal "The value is 123", string
   end
+  # interpolate -> insert
 
   def test_single_quoted_strings_do_not_interpolate
     value = 123
     string = 'The value is #{value}'
-    assert_equal __, string
+    assert_equal 'The value is #{value}', string
   end
 
   def test_any_ruby_expression_may_be_interpolated
     string = "The square root of 5 is #{Math.sqrt(5)}"
-    assert_equal __, string
+    assert_equal "The square root of 5 is #{Math.sqrt(5)}", string
   end
 
   def test_you_can_get_a_substring_from_a_string
     string = "Bacon, lettuce and tomato"
-    assert_equal __, string[7,3]
-    assert_equal __, string[7..9]
+    assert_equal "let", string[7,3] # Ab dem 7 Zeichen 3 Zeichen inc. dem siebten.
+    assert_equal "let", string[7..9] # Alle Zeichen von 7 bis 9 (inc. 7 und 9)
   end
 
   def test_you_can_get_a_single_character_from_a_string
     string = "Bacon, lettuce and tomato"
-    assert_equal __, string[1]
+    assert_equal "a", string[1]
 
     # Surprised?
+    # Strings get stored in an "array"?
   end
 
   in_ruby_version("1.8") do
     def test_in_older_ruby_single_characters_are_represented_by_integers
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal 97, ?a
+      assert_equal true, ?a == 97
 
-      assert_equal __, ?b == (?a + 1)
+      assert_equal true, ?b == (?a + 1)
     end
   end
 
   in_ruby_version("1.9", "2") do
     def test_in_modern_ruby_single_characters_are_represented_by_strings
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal "a", ?a
+      assert_equal false, ?a == 97
     end
   end
 
   def test_strings_can_be_split
     string = "Sausage Egg Cheese"
     words = string.split
-    assert_equal [__, __, __], words
+    assert_equal ["Sausage","Egg","Cheese"], words
   end
 
   def test_strings_can_be_split_with_different_patterns
     string = "the:rain:in:spain"
     words = string.split(/:/)
-    assert_equal [__, __, __, __], words
+    assert_equal ["the","rain","in","spain"], words
 
     # NOTE: Patterns are formed from Regular Expressions.  Ruby has a
     # very powerful Regular Expression library.  We will become
@@ -184,14 +223,14 @@ EOS
 
   def test_strings_can_be_joined
     words = ["Now", "is", "the", "time"]
-    assert_equal __, words.join(" ")
+    assert_equal "Now is the time", words.join(" ")
   end
 
   def test_strings_are_unique_objects
     a = "a string"
     b = "a string"
 
-    assert_equal __, a           == b
-    assert_equal __, a.object_id == b.object_id
+    assert_equal true, a           == b
+    assert_equal false, a.object_id == b.object_id  # why?
   end
 end
